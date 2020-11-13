1a544bc8ae3df81fc6f2cb310c08537aa5eac750
shivangijb <37670360+shivangijb@users.noreply.github.com>
2020-11-13 15:05:48 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+
+    relatives = ["Bhanu","Prashansha","Rashi"];
+    render() {
+
+        return(
+            <ol key="relativeList">
+                 {this.relatives.map((relative , index) => (
+                    <li key={`relativeListItem${index+1}`}>{relative}</li>
+                    ))}
+            </ol>
+        )
+    }
+}
+
+
+export default App;
